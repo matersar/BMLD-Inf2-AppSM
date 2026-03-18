3df86ec27bad4bd35f0afd2222239c773ba3f97e
Sarah <matersar@students.zhaw.ch>
2026-03-18 11:21:00 +0100

    Anpassung

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-
+import pandas as pd
 # --- NEW CODE: import and initialize data manager and login manager ---
 from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
 
 data_manager = DataManager(       # initialize data manager
     fs_protocol='webdav',         # protocol for the filesystem, use webdav for switch drive
-    fs_root_folder="BMLD_App_DB"  # folder on switch drive where the data is stored
+    fs_root_folder="Informatik_2_App"  # folder on switch drive where the data is stored
     ) 
 login_manager = LoginManager(data_manager) # handles user login and registration
 login_manager.login_register()             # stops if not logged in
