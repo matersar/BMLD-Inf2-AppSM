8b8ab1db46f06b00377e4a74ffd7648205d0047c
Sarah <matersar@students.zhaw.ch>
2026-04-26 08:24:04 +0200

    Add exercise data and training plan page

---
+++ utils/exercise_data.py
@@ -0,0 +1,51 @@
+EXERCISES = [
+    # Rücken
+    {"name": "Latziehen", "muskelgruppe": "Rücken", "level": "Anfänger"},
+    {"name": "Klimmzüge", "muskelgruppe": "Rücken", "level": "Fortgeschritten"},
+    {"name": "Rudern Maschine", "muskelgruppe": "Rücken", "level": "Anfänger"},
+    {"name": "Langhantelrudern", "muskelgruppe": "Rücken", "level": "Fortgeschritten"},
+    {"name": "Einarmiges Kurzhantelrudern", "muskelgruppe": "Rücken", "level": "Anfänger"},
+    {"name": "Face Pulls", "muskelgruppe": "Rücken", "level": "Anfänger"},
+    {"name": "Reverse Flys", "muskelgruppe": "Rücken", "level": "Anfänger"},
+    {"name": "Superman", "muskelgruppe": "Rücken", "level": "Anfänger"},
+
+    # Arme
+    {"name": "Bizepscurls", "muskelgruppe": "Arme", "level": "Anfänger"},
+    {"name": "Hammer Curls", "muskelgruppe": "Arme", "level": "Anfänger"},
+    {"name": "Konzentrationscurls", "muskelgruppe": "Arme", "level": "Anfänger"},
+    {"name": "Trizepsdrücken Kabel", "muskelgruppe": "Arme", "level": "Anfänger"},
+    {"name": "Dips", "muskelgruppe": "Arme", "level": "Fortgeschritten"},
+    {"name": "Trizeps Kickbacks", "muskelgruppe": "Arme", "level": "Anfänger"},
+    {"name": "Enges Bankdrücken", "muskelgruppe": "Arme", "level": "Fortgeschritten"},
+    {"name": "Overhead Trizeps Extension", "muskelgruppe": "Arme", "level": "Anfänger"},
+
+    # Po
+    {"name": "Hip Thrust", "muskelgruppe": "Po", "level": "Anfänger"},
+    {"name": "Glute Bridge", "muskelgruppe": "Po", "level": "Anfänger"},
+    {"name": "Bulgarian Split Squats", "muskelgruppe": "Po", "level": "Fortgeschritten"},
+    {"name": "Kickbacks Kabel", "muskelgruppe": "Po", "level": "Anfänger"},
+    {"name": "Step-Ups", "muskelgruppe": "Po", "level": "Anfänger"},
+    {"name": "Sumo Squats", "muskelgruppe": "Po", "level": "Anfänger"},
+    {"name": "Kreuzheben", "muskelgruppe": "Po", "level": "Fortgeschritten"},
+    {"name": "Abduktoren Maschine", "muskelgruppe": "Po", "level": "Anfänger"},
+
+    # Beine
+    {"name": "Kniebeugen", "muskelgruppe": "Beine", "level": "Anfänger"},
+    {"name": "Beinpresse", "muskelgruppe": "Beine", "level": "Anfänger"},
+    {"name": "Ausfallschritte", "muskelgruppe": "Beine", "level": "Anfänger"},
+    {"name": "Beinstrecker", "muskelgruppe": "Beine", "level": "Anfänger"},
+    {"name": "Beinbeuger", "muskelgruppe": "Beine", "level": "Anfänger"},
+    {"name": "Wadenheben", "muskelgruppe": "Beine", "level": "Anfänger"},
+    {"name": "Front Squats", "muskelgruppe": "Beine", "level": "Fortgeschritten"},
+    {"name": "Jump Squats", "muskelgruppe": "Beine", "level": "Fortgeschritten"},
+
+    # Bauch
+    {"name": "Crunches", "muskelgruppe": "Bauch", "level": "Anfänger"},
+    {"name": "Sit-Ups", "muskelgruppe": "Bauch", "level": "Anfänger"},
+    {"name": "Plank", "muskelgruppe": "Bauch", "level": "Anfänger"},
+    {"name": "Russian Twists", "muskelgruppe": "Bauch", "level": "Anfänger"},
+    {"name": "Leg Raises", "muskelgruppe": "Bauch", "level": "Anfänger"},
+    {"name": "Mountain Climbers", "muskelgruppe": "Bauch", "level": "Anfänger"},
+    {"name": "Bicycle Crunches", "muskelgruppe": "Bauch", "level": "Anfänger"},
+    {"name": "Hanging Leg Raises", "muskelgruppe": "Bauch", "level": "Fortgeschritten"},
+]
