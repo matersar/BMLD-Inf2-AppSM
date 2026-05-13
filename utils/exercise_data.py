e853c5789e53d15df6d86bb771604d7cc5016b77
Sarah <matersar@students.zhaw.ch>
2026-05-13 10:21:58 +0200

    upgrade exercise

--- utils/exercise_data.py
+++ utils/exercise_data.py
@@ -1,51 +1,62 @@
 EXERCISES = [
-    # Rücken
+    # Rücken - Gym
     {"name": "Latziehen", "muskelgruppe": "Rücken", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
-    {"name": "Klimmzüge", "muskelgruppe": "Rücken", "level": "Fortgeschritten", "ort": "Gym", "saetze": 3, "wiederholungen": "8-10"},
     {"name": "Rudern Maschine", "muskelgruppe": "Rücken", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
-    {"name": "Langhantelrudern", "muskelgruppe": "Rücken", "level": "Fortgeschritten", "ort": "Gym", "saetze": 4, "wiederholungen": "8-10"},
     {"name": "Einarmiges Kurzhantelrudern", "muskelgruppe": "Rücken", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
-    {"name": "Face Pulls", "muskelgruppe": "Rücken", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "12-15"},
-    {"name": "Reverse Flys", "muskelgruppe": "Rücken", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "12-15"},
+    {"name": "Klimmzüge", "muskelgruppe": "Rücken", "level": "Fortgeschritten", "ort": "Gym", "saetze": 3, "wiederholungen": "8-10"},
+    {"name": "Langhantelrudern", "muskelgruppe": "Rücken", "level": "Fortgeschritten", "ort": "Gym", "saetze": 4, "wiederholungen": "8-10"},
+
+    # Rücken - Home
     {"name": "Superman", "muskelgruppe": "Rücken", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "30 Sek"},
+    {"name": "Reverse Snow Angels", "muskelgruppe": "Rücken", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "12"},
+    {"name": "Bird Dog", "muskelgruppe": "Rücken", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "10 pro Seite"},
+    {"name": "Y-T-W Raises", "muskelgruppe": "Rücken", "level": "Fortgeschritten", "ort": "Home", "saetze": 3, "wiederholungen": "12"},
 
-    # Arme
+    # Arme - Gym
     {"name": "Bizepscurls", "muskelgruppe": "Arme", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
-    {"name": "Hammer Curls", "muskelgruppe": "Arme", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
-    {"name": "Konzentrationscurls", "muskelgruppe": "Arme", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
     {"name": "Trizepsdrücken Kabel", "muskelgruppe": "Arme", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
+    {"name": "Hammer Curls", "muskelgruppe": "Arme", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
     {"name": "Dips", "muskelgruppe": "Arme", "level": "Fortgeschritten", "ort": "Gym", "saetze": 3, "wiederholungen": "8-10"},
-    {"name": "Trizeps Kickbacks", "muskelgruppe": "Arme", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "12"},
-    {"name": "Enges Bankdrücken", "muskelgruppe": "Arme", "level": "Fortgeschritten", "ort": "Gym", "saetze": 4, "wiederholungen": "8-10"},
-    {"name": "Overhead Trizeps Extension", "muskelgruppe": "Arme", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
 
-    # Po
+    # Arme - Home
+    {"name": "Enge Liegestütze", "muskelgruppe": "Arme", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "8-12"},
+    {"name": "Trizeps Dips am Stuhl", "muskelgruppe": "Arme", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "10-12"},
+    {"name": "Plank Shoulder Taps", "muskelgruppe": "Arme", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "20"},
+    {"name": "Diamond Push-ups", "muskelgruppe": "Arme", "level": "Fortgeschritten", "ort": "Home", "saetze": 3, "wiederholungen": "8-10"},
+
+    # Po - Gym
     {"name": "Hip Thrust", "muskelgruppe": "Po", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
-    {"name": "Glute Bridge", "muskelgruppe": "Po", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "12"},
-    {"name": "Bulgarian Split Squats", "muskelgruppe": "Po", "level": "Fortgeschritten", "ort": "Home", "saetze": 3, "wiederholungen": "10"},
     {"name": "Kickbacks Kabel", "muskelgruppe": "Po", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "12"},
-    {"name": "Step-Ups", "muskelgruppe": "Po", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "10"},
-    {"name": "Sumo Squats", "muskelgruppe": "Po", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "10-12"},
-    {"name": "Kreuzheben", "muskelgruppe": "Po", "level": "Fortgeschritten", "ort": "Gym", "saetze": 4, "wiederholungen": "6-8"},
     {"name": "Abduktoren Maschine", "muskelgruppe": "Po", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "12-15"},
+    {"name": "Kreuzheben", "muskelgruppe": "Po", "level": "Fortgeschritten", "ort": "Gym", "saetze": 4, "wiederholungen": "6-8"},
 
-    # Beine
-    {"name": "Kniebeugen", "muskelgruppe": "Beine", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "10-12"},
+    # Po - Home
+    {"name": "Glute Bridge", "muskelgruppe": "Po", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "12"},
+    {"name": "Donkey Kicks", "muskelgruppe": "Po", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "12 pro Seite"},
+    {"name": "Fire Hydrants", "muskelgruppe": "Po", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "12 pro Seite"},
+    {"name": "Bulgarian Split Squats", "muskelgruppe": "Po", "level": "Fortgeschritten", "ort": "Home", "saetze": 3, "wiederholungen": "10 pro Seite"},
+
+    # Beine - Gym
     {"name": "Beinpresse", "muskelgruppe": "Beine", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
-    {"name": "Ausfallschritte", "muskelgruppe": "Beine", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "10"},
     {"name": "Beinstrecker", "muskelgruppe": "Beine", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "12"},
     {"name": "Beinbeuger", "muskelgruppe": "Beine", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "12"},
-    {"name": "Wadenheben", "muskelgruppe": "Beine", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "15"},
     {"name": "Front Squats", "muskelgruppe": "Beine", "level": "Fortgeschritten", "ort": "Gym", "saetze": 4, "wiederholungen": "6-8"},
+
+    # Beine - Home
+    {"name": "Kniebeugen", "muskelgruppe": "Beine", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "10-12"},
+    {"name": "Ausfallschritte", "muskelgruppe": "Beine", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "10 pro Seite"},
+    {"name": "Wadenheben", "muskelgruppe": "Beine", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "15"},
     {"name": "Jump Squats", "muskelgruppe": "Beine", "level": "Fortgeschritten", "ort": "Home", "saetze": 3, "wiederholungen": "10"},
 
-    # Bauch
+    # Bauch - Gym
+    {"name": "Crunch Maschine", "muskelgruppe": "Bauch", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "12"},
+    {"name": "Cable Crunches", "muskelgruppe": "Bauch", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "12"},
+    {"name": "Hanging Knee Raises", "muskelgruppe": "Bauch", "level": "Anfänger", "ort": "Gym", "saetze": 3, "wiederholungen": "10-12"},
+    {"name": "Hanging Leg Raises", "muskelgruppe": "Bauch", "level": "Fortgeschritten", "ort": "Gym", "saetze": 3, "wiederholungen": "10"},
+
+    # Bauch - Home
     {"name": "Crunches", "muskelgruppe": "Bauch", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "15"},
-    {"name": "Sit-Ups", "muskelgruppe": "Bauch", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "15"},
     {"name": "Plank", "muskelgruppe": "Bauch", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "30 Sek"},
     {"name": "Russian Twists", "muskelgruppe": "Bauch", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "20"},
-    {"name": "Leg Raises", "muskelgruppe": "Bauch", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "12"},
     {"name": "Mountain Climbers", "muskelgruppe": "Bauch", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "30 Sek"},
-    {"name": "Bicycle Crunches", "muskelgruppe": "Bauch", "level": "Anfänger", "ort": "Home", "saetze": 3, "wiederholungen": "20"},
-    {"name": "Hanging Leg Raises", "muskelgruppe": "Bauch", "level": "Fortgeschritten", "ort": "Gym", "saetze": 3, "wiederholungen": "10"},
 ]
