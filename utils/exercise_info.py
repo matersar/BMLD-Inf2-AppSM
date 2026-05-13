d7b9340c723ce42aaf93c4121a1dd217bd8e64d3
Sarah <matersar@students.zhaw.ch>
2026-05-13 16:02:17 +0200

    add exercise info

---
+++ utils/exercise_info.py
@@ -0,0 +1,45 @@
+EXERCISE_INFO = {
+
+    "Beinpresse": {
+        "bild": "images/beinpresse.png",
+        "beschreibung": "Drücke die Plattform kontrolliert nach vorne.",
+        "tipp": "Knie nicht komplett durchstrecken."
+    },
+
+    "Front Squats": {
+        "bild": "images/front_squats.png",
+        "beschreibung": "Halte die Hantel vorne auf den Schultern.",
+        "tipp": "Rücken gerade halten."
+    },
+
+    "Hip Thrust": {
+        "bild": "images/hip_thrust.png",
+        "beschreibung": "Hebe die Hüfte kontrolliert nach oben.",
+        "tipp": "Spanne den Po oben bewusst an."
+    },
+
+    "Latziehen": {
+        "bild": "images/latziehen.png",
+        "beschreibung": "Ziehe die Stange kontrolliert zur Brust.",
+        "tipp": "Nicht mit Schwung arbeiten."
+    },
+
+    "Rudern Maschine": {
+        "bild": "images/rudern_maschine.png",
+        "beschreibung": "Ziehe die Griffe kontrolliert nach hinten.",
+        "tipp": "Schultern unten halten."
+    },
+
+    "Beinstrecker": {
+        "bild": "images/beinstrecker.png",
+        "beschreibung": "Strecke die Beine langsam nach vorne.",
+        "tipp": "Nicht mit Schwung trainieren."
+    },
+
+    "Plank": {
+        "bild": "images/plank.png",
+        "beschreibung": "Halte den Körper stabil in einer Linie.",
+        "tipp": "Bauch anspannen und Rücken gerade halten."
+    }
+
+}
